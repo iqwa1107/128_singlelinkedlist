45f911ca4992e8bb473040dd1c4701784562a749
iqwa1107 <ramahs49@gmail.com>
2023-05-18 18:57:36 +0700

    memasukan parameter fungsi

--- Singlelinkedlist_128/Singlelinkedlist_128.cpp
+++ Singlelinkedlist_128/Singlelinkedlist_128.cpp
@@ -4,5 +4,20 @@ using namespace std;
 struct Node {
 	int noMhs;
 	string name;
-	Node next;
+	Node* next;
 };
+
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+}
